4e7e946e1134c2f1ae9cf7634479cd6e9010f88e
Steve <stevebotella@hotmail.fr>
2021-11-26 13:57:19 +0100

    feat: first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+
+
+def main():
+    this_folder = os.path.dirname(os.path.abspath(__file__))
+    text_file = os.path.join(this_folder, 'text.txt')
+    run_options = True
+
+    while run_options:
+        text_check = input("Choose action : create, add, read or quit")
+        if text_check == "create":
+            create_text = input("Your text : ")
+            file = open("text.txt", "w")
+            file.write(f"{create_text} \n")
+            file.close()
+        if text_check == "add":
+            add_text = input("Your text : ")
+            file = open("text.txt", "a")
+            file.write(add_text + "\n")
+            file.close()
+        if text_check == "read":
+            file = open("text.txt", "r")
+            read = file.readlines()
+            file.close()
+            line_number = -1
+            input_next = True
+            while input_next:
+                next_line = input("Press S to display next line or Q to quit")
+                if next_line == "s":
+                    line_number = line_number + 1
+                    print(read[line_number])
+                if next_line == "q":
+                    input_next = False
+
+        if text_check == "quit":
+            run_options = False
+
+
+if __name__ == '__main__':
+    main()
